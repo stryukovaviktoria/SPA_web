a66e3f91bf3386a88ad360c9531289651c3c5c41
stryukovaviktoria <155023346+stryukovaviktoria@users.noreply.github.com>
2026-03-29 18:36:51 +0300

    Add files via upload

--- src/app/features/products/product-list/product-list.ts
+++ src/app/features/products/product-list/product-list.ts
@@ -1,18 +1,46 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { PRODUCTS } from '../../../shared/mock-data';
 import { ProductCardComponent } from '../../../shared/components/product-card/product-card';
 
 @Component({
   selector: 'cosm-list',
   standalone: true,
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, FormsModule],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css'
 })
 export class ProductListComponent {
-  public items = PRODUCTS;
+  public allProducts = PRODUCTS;
+  public filteredProducts = PRODUCTS;
+
+  public searchQuery: string = '';
+  public selectedCategory: string = 'All';
+
+  filterItems(): void {
+    const text = this.searchQuery.toLowerCase();
+
+    this.filteredProducts = this.allProducts.filter(item => {
+      const matchesText = item.title.toLowerCase().includes(text);
+
+      const matchesCategory =
+        this.selectedCategory === 'All' ||
+        item.category === this.selectedCategory;
+
+      return matchesText && matchesCategory;
+    });
+  }
 
   handleCardAction(id: number): void {
     console.log('Користувач натиснув кнопку на товарі з ID:', id);
+  }
+
+  resetFilters(input: HTMLInputElement): void {
+    this.searchQuery = '';
+    this.selectedCategory = 'All';
+    this.filteredProducts = this.allProducts;
+
+    input.value = '';
+    input.focus();
   }
 }
